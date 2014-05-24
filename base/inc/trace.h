48d061059f5a8487b5498aa140d3164284238312
oscarcai <84981517@qq.com>
2014-05-25 00:07:18 +0800

    Adjust trace levels

--- base/inc/trace.h
+++ base/inc/trace.h
@@ -13,11 +13,9 @@
 #ifndef __DVM_TRACE_H__
 #define __DVM_TRACE_H__
 
-
 #ifdef WIN32
 #include <stdio.h>
 #endif
-
 
 #ifdef __cplusplus
 extern "C" {
@@ -25,25 +23,24 @@ extern "C" {
 
 /**
  * TRACE_LEV value definitions:
- * 1 - error
- * 2 - warning
- * 2 - debug
- * 4 - info
+ * 1 - debug
+ * 2 - info
+ * 3 - warning
+ * 4 - error
  */
 #ifndef TRACE_LEV 
-#define TRACE_LEV 2
+#define TRACE_LEV 4
 #endif
 
-#define DVMTraceErr (TRACE_LEV < 1) ? (void) 0 : (void)DthingTraceE
-#define DVMTraceWar (TRACE_LEV < 2) ? (void) 0 : (void)DthingTraceW
-#define DVMTraceDbg (TRACE_LEV < 3) ? (void) 0 : (void)DthingTraceD
-#define DVMTraceInf (TRACE_LEV < 4) ? (void) 0 : (void)DthingTraceI
+#define DVMTraceDbg (TRACE_LEV < 1) ? (void) 0 : (void)DthingTraceD
+#define DVMTraceInf (TRACE_LEV < 2) ? (void) 0 : (void)DthingTraceI
+#define DVMTraceWar (TRACE_LEV < 3) ? (void) 0 : (void)DthingTraceW
+#define DVMTraceErr (TRACE_LEV < 4) ? (void) 0 : (void)DthingTraceE
 
-
-void DthingTraceE(const char * fmt,...);
-void DthingTraceW(const char * fmt,...);
 void DthingTraceD(const char * fmt,...);
 void DthingTraceI(const char * fmt,...);
+void DthingTraceW(const char * fmt,...);
+void DthingTraceE(const char * fmt,...);
 
 #ifdef __cplusplus
 }
--- base/src/trace.c
+++ base/src/trace.c
@@ -5,104 +5,63 @@
 #include "trace.h"
 #include "crtl.h"
 
-#define MSG_BUFFER_LEN  256
+#define MSG_BUFFER_LEN  255
 
-void DthingTraceE(const char * fmt,...)
+void DthingTrace(const char* tag, const char* fmt, va_list argList)
 {
-    char msg_buffer[MSG_BUFFER_LEN + 1] = {0};
-    va_list arg_list = {0};
-    CRTL_memset(msg_buffer, 0, MSG_BUFFER_LEN);
-    sprintf(msg_buffer, "[DTHING Error]:");
-                
-    va_start(arg_list, fmt);/*lint !e718 !e64*/
-    _vsnprintf(msg_buffer+CRTL_strlen(msg_buffer),MSG_BUFFER_LEN - strlen(msg_buffer), fmt, arg_list);
+	char msgBuf[MSG_BUFFER_LEN + 1] = {0};
+	int len = 0;
+
+	CRTL_memset(msgBuf, 0, sizeof(msgBuf));
+	CRTL_sprintf(msgBuf, tag);
+	len = CRTL_strlen(msgBuf);
+	_vsnprintf(msgBuf + len, MSG_BUFFER_LEN - len, fmt, argList);
 
 #if defined(ARCH_X86)
-    printf(msg_buffer);
+	CRTL_printf(msgBuf);
 #elif defined(ARCH_ARM_SPD)
-    #if defined(WIN32)
-        SCI_Trace(msg_buffer);
-    #else
-        SCI_TraceLow(msg_buffer);
-    #endif    
+	#if defined(WIN32)
+		SCI_Trace(msgBuf);
+	#else
+		SCI_TraceLow(msgBuf);
+	#endif    
 #else
-    #error "unsupport!"
+	#error "unsupport!"
 #endif
-
-    va_end(arg_list);    
-}
-
-void DthingTraceW(const char * fmt,...)
-{
-    char msg_buffer[MSG_BUFFER_LEN + 1] = {0};
-    va_list arg_list = {0};
-    CRTL_memset(msg_buffer, 0, MSG_BUFFER_LEN);
-    sprintf(msg_buffer, "[DTHING Warning]:");
-                
-    va_start(arg_list, fmt);/*lint !e718 !e64*/
-    _vsnprintf(msg_buffer+CRTL_strlen(msg_buffer),MSG_BUFFER_LEN - strlen(msg_buffer), fmt, arg_list);
-
-#if defined(ARCH_X86)
-    printf(msg_buffer);
-#elif defined(ARCH_ARM_SPD)
-    #if defined(WIN32)
-        SCI_Trace(msg_buffer);
-    #else
-        SCI_TraceLow(msg_buffer);
-    #endif    
-#else
-    #error "unsupport!"
-#endif
-
-    va_end(arg_list);    
 }
 
 void DthingTraceD(const char * fmt,...)
 {
-    char msg_buffer[MSG_BUFFER_LEN + 1] = {0};
-    va_list arg_list = {0};
-    CRTL_memset(msg_buffer, 0, MSG_BUFFER_LEN);
-    sprintf(msg_buffer, "[DTHING Debug]:");
-                
-    va_start(arg_list, fmt);/*lint !e718 !e64*/
-    _vsnprintf(msg_buffer+CRTL_strlen(msg_buffer),MSG_BUFFER_LEN - strlen(msg_buffer), fmt, arg_list);
+	va_list argList = {0};
 
-#if defined(ARCH_X86)
-    printf(msg_buffer);
-#elif defined(ARCH_ARM_SPD)
-    #if defined(WIN32)
-        SCI_Trace(msg_buffer);
-    #else
-        SCI_TraceLow(msg_buffer);
-    #endif    
-#else
-    #error "unsupport!"
-#endif
-
-    va_end(arg_list);    
+	va_start(argList, fmt);/*lint !e718 !e64*/
+	DthingTrace("[DTHING Debug]:", fmt, argList);
+	va_end(argList);    
 }
 
 void DthingTraceI(const char * fmt,...)
 {
-    char msg_buffer[MSG_BUFFER_LEN + 1] = {0};
-    va_list arg_list = {0};
-    CRTL_memset(msg_buffer, 0, MSG_BUFFER_LEN);
-    sprintf(msg_buffer, "[DTHING Info]:");
-                
-    va_start(arg_list, fmt);/*lint !e718 !e64*/
-    _vsnprintf(msg_buffer+CRTL_strlen(msg_buffer),MSG_BUFFER_LEN - strlen(msg_buffer), fmt, arg_list);
+	va_list argList = {0};
 
-#if defined(ARCH_X86)
-    printf(msg_buffer);
-#elif defined(ARCH_ARM_SPD)
-    #if defined(WIN32)
-        SCI_Trace(msg_buffer);
-    #else
-        SCI_TraceLow(msg_buffer);
-    #endif    
-#else
-    #error "unsupport!"
-#endif
+	va_start(argList, fmt);/*lint !e718 !e64*/
+	DthingTrace("[DTHING Info]:", fmt, argList);
+	va_end(argList);    
+}
 
-    va_end(arg_list);    
+void DthingTraceW(const char * fmt,...)
+{
+	va_list argList = {0};
+
+	va_start(argList, fmt);/*lint !e718 !e64*/
+	DthingTrace("[DTHING Warning]:", fmt, argList);
+	va_end(argList);    
+}
+
+void DthingTraceE(const char * fmt,...)
+{
+	va_list argList = {0};
+
+	va_start(argList, fmt);/*lint !e718 !e64*/
+	DthingTrace("[DTHING Error]:", fmt, argList);
+	va_end(argList);    
 }
