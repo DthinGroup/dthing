743f16cd995a2bb2d19037e51eecaa783092e520
fei <fei.ming@yarlungsoft.com>
2014-10-31 13:43:08 +0800

    add native implementation for raw keypad driver

---
+++ vm/impl/nativeRawKeyPdDriver.c
@@ -0,0 +1,45 @@
+#include <vm_common.h>
+#include "nativeRawKeyPdDriver.h"
+
+/**
+ * Class:     iot_oem_kpd_RawKeyPdDriver
+ * Method:    init0
+ * Signature: ()I
+ */
+void Java_iot_oem_kpd_RawKeyPdDriver_init0(const u4* args, JValue* pResult) {
+    ClassObject* thisObj = (ClassObject*) args[0];
+    jint ret = 0;
+
+    // TODO: implementation
+
+    RETURN_INT(ret);
+}
+
+/**
+ * Class:     iot_oem_kpd_RawKeyPdDriver
+ * Method:    getKey0
+ * Signature: ()J
+ */
+void Java_iot_oem_kpd_RawKeyPdDriver_getKey0(const u4* args, JValue* pResult) {
+    ClassObject* thisObj = (ClassObject*) args[0];
+    jlong ret = 0;
+
+    // TODO: implementation
+
+    RETURN_LONG(ret);
+}
+
+/**
+ * Class:     iot_oem_kpd_RawKeyPdDriver
+ * Method:    close0
+ * Signature: ()I
+ */
+void Java_iot_oem_kpd_RawKeyPdDriver_close0(const u4* args, JValue* pResult) {
+    ClassObject* thisObj = (ClassObject*) args[0];
+    jint ret = 0;
+
+    // TODO: implementation
+
+    RETURN_INT(ret);
+}
+
