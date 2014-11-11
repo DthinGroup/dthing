5d30a7e69b7bb6d5e5c161a4369f457b8538fe15
nix <longfan425@126.com>
2014-11-11 10:27:56 +0800

    at & sms Ams module, TODO

---
+++ rams/inc/ams_at.h
@@ -0,0 +1,19 @@
+#ifndef __AMS_AT_H__
+#define __AMS_AT_H__
+
+#include <std_global.h>
+#include <eventbase.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //__AMS_H__
---
+++ rams/inc/ams_sms.h
@@ -0,0 +1,19 @@
+#ifndef __AMS_SMS_H__
+#define __AMS_SMS_H__
+
+#include <std_global.h>
+#include <eventbase.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //__AMS_H__
