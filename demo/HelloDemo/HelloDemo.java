e2bcc35ad64c448427e6aaae24030f86442a8ad3
fei <fei.ming@yarlungsoft.com>
2014-11-30 23:55:51 +0800

    [demo] report test info to server

--- demo/HelloDemo/HelloDemo.java
+++ demo/HelloDemo/HelloDemo.java
@@ -1,9 +1,16 @@
 import java.io.PrintStream;
 import jp.co.cmcc.event.Applet;
 import jp.co.cmcc.event.Event;
+import java.net.http.HttpURLConnection;
+import java.net.http.URL;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class HelloDemo extends Applet
 {
+    private static boolean allowLogPrint = true;
+    private static final String REPORT_SERVER_FORMAT = "http://42.121.18.62:8080/dthing/ParmInfo.action?saveDataInfo&saveType=log&parmInfo=";
+
     public HelloDemo()
     {
         System.out.println("[HelloDemo] constructor is called!");
@@ -38,5 +45,27 @@ public class HelloDemo extends Applet
         String str2 = System.getProperty("property2", "value2");
         System.out.println("[HelloDemo] key = property2 | value = " + str2);
         Runtime.getRuntime().gc();
+
+	 try {
+            reportTestInfo("[Hello] Test.OK");
+	 } catch (IOException  e) {
+	     System.out.println("[Hello] IOException:" + e);
+	 }
+    }
+
+    private void reportTestInfo(String info) throws IOException {
+        String content = "Hello:" + info;
+        String reportInfo = REPORT_SERVER_FORMAT + info;
+
+        if (allowLogPrint) {
+            System.out.println("[Hello][Test Result]" + content);
+        }
+
+        URL url = new URL(reportInfo);
+        HttpURLConnection httpConn = (HttpURLConnection)url.openConnection();
+        httpConn.setRequestMethod(HttpURLConnection.POST);
+        InputStream dis = httpConn.getInputStream();
+        dis.close();
+        httpConn.disconnect();
     }
 }
