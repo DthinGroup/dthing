3f2c6935e8d5f7fe56f3c589587797e74d14c30e
fei <superfei2002@163.com>
2015-02-06 19:20:08 +0800

    [dvm] Add build guide

--- build.bat
+++ build.bat
@@ -1,16 +1,16 @@
 REM #######################################################################
-REM Copyright [2013] - [2014] Yarlungsoft 
-REM 
-REM Licensed under the Apache License, Version 2.0 (the "License"); 
-REM you may not use this file except in compliance with the License. 
-REM You may obtain a copy of the License at 
-REM 
-REM http://www.apache.org/licenses/LICENSE-2.0 
-REM 
-REM Unless required by applicable law or agreed to in writing, software 
-REM distributed under the License is distributed on an "AS IS" BASIS, 
-REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-REM See the License for the specific language governing permissions and 
+REM Copyright [2013] - [2014] Yarlungsoft
+REM
+REM Licensed under the Apache License, Version 2.0 (the "License");
+REM you may not use this file except in compliance with the License.
+REM You may obtain a copy of the License at
+REM
+REM http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
 REM limitations under the License.
 REM #######################################################################
 
@@ -46,11 +46,12 @@ for %%i in (%*) do (
     ) else if /i "%%i" EQU "sim" (
         set BUILDARCH=msvc
     ) else (
-        @echo "Unknown parameter: %%i"
+        echo "Unknown parameter: %%i"
         goto HELP
     )
 )
 
+:BUILD
 if "%CLEANBUILD%" EQU "TRUE" (
     gmake TOOLCHAIN=%BUILDARCH% clean
 )
@@ -65,7 +66,7 @@ if "%BINSBUILD%" EQU "TRUE" (
 )
 
 if "%BUILDARCH%" EQU "rvct" (
-	call rvct_release.bat
+  call rvct_release.bat
 )
 
 goto EXIT;
@@ -88,6 +89,51 @@ echo        build lib   - only start lib build, no clean
 echo        build clean bin sim - clean first, then start bin build on sim
 echo        build clean lib arm - clean first, then start lib build on arm
 echo ####################################################################
+
+:BUILD_GUIDE
+echo Entering build guide mode...                                 
+echo ---------------------------
+echo [0] normal build
+echo [1] clean build
+echo ---------------------------
+echo Please select build mode and press Enter for confirmation...
+SET /P BUILD_MODE_ID=
+
+if "%BUILD_MODE_ID%"=="1" (
+    set CLEANBUILD=TRUE
+) else (
+    set CLEANBUILD=FALSE
+)
+
+echo ---------------------------
+echo [0] library
+echo [1] binary
+echo ---------------------------
+echo Please select build output and press Enter for confirmation...
+SET /P BUILD_OUTPUT_ID=
+
+if "%BUILD_OUTPUT_ID%"=="1" (
+    set BINSBUILD=TRUE
+) else (
+    set BINSBUILD=FALSE
+)
+
+echo ---------------------------
+echo [0] arm
+echo [1] sim (x86)
+echo ---------------------------
+echo Please select build arch format and press Enter for confirmation...
+SET /P BUILD_ARCH_ID=
+
+if "%BUILD_ARCH_ID%"=="1" (
+    set BUILDARCH=msvc
+    XCOPY /R /Y vm\inc\x86\vm_macros.h vm\inc\
+) else (
+    set BUILDARCH=rvct
+    XCOPY /R /Y vm\inc\arm\vm_macros.h vm\inc\
+)
+
+goto BUILD;
 
 :EXIT
 
