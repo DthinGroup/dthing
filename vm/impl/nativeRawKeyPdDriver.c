d03bce0bf490d05e53e5a5cd3458d47c72610eca
fei <superfei2002@163.com>
2015-03-31 16:01:47 +0800

    [keypad] native implementation change

--- vm/impl/nativeRawKeyPdDriver.c
+++ vm/impl/nativeRawKeyPdDriver.c
@@ -4,10 +4,14 @@
 #if defined(ARCH_ARM_SPD)
 #include "sci_types.h"
 #include "os_api.h"
-#include "Keypad.h"
 #include "AsyncIO.h"
 
+#include "keypad.h"
+#include "dal_keypad.h"
+#include "sci_service.h"
+
 static ASYNC_Notifier *g_keyEventNotifier = NULL;
+void kpd_callback (uint32 id, uint32 argc, void *argv);
 static jlong convertNativeKeyToJava(KPDSVR_SIG_T *key_sig_ptr);
 #endif //ARCH_ARM_SPD
 
@@ -20,7 +24,14 @@ void Java_iot_oem_kpd_RawKeyPdDriver_init0(const u4* args, JValue* pResult) {
     ClassObject* thisObj = (ClassObject*) args[0];
     jint ret = 0;
 
-    // TODO: implementation
+    DthingTraceD("keypad: init and register key message callback\n");
+
+#if defined(ARCH_ARM_SPD)
+    SCI_RegisterMsg(KEYPAD_SERVICE,
+                   (uint8)(KPD_DOWN & 0xff),
+                   (uint8)(KPD_UP & 0xff),
+                   kpd_callback);
+#endif //ARCH_ARM_SPD
 
     RETURN_INT(ret);
 }
@@ -35,26 +46,17 @@ void Java_iot_oem_kpd_RawKeyPdDriver_getKey0(const u4* args, JValue* pResult) {
     jlong ret = 0;
 
 #if defined(ARCH_ARM_SPD)
-    KPDSVR_SIG_T *key_sig_ptr = 0x0;
 
     if (AsyncIO_firstCall())
     {
         g_keyEventNotifier =  Async_getCurNotifier();
-        key_sig_ptr = (KPDSVR_SIG_T *)SCI_GetSignal(KPDSVR);
-
-        if (key_sig_ptr == 0x0)
-        {
-            goto end;
-        }
-
-        if(key_sig_ptr->key < KPD_VK_NUM)
-        {
-            //Convert Key
-            ret = convertNativeKeyToJava(key_sig_ptr);
-        }
-
-        SCI_FREE(key_sig_ptr);
-        key_sig_ptr = 0x0;
+        DthingTraceD("keypad: ask for key and waiting\n");
+        AsyncIO_callAgainWhenSignalled();
+    }
+    else
+    {
+        ret = g_keyEventNotifier->notified;
+        DthingTraceD("keypad: get key code %d\n", ret);
     }
 
 end:
@@ -74,12 +76,59 @@ void Java_iot_oem_kpd_RawKeyPdDriver_close0(const u4* args, JValue* pResult) {
     ClassObject* thisObj = (ClassObject*) args[0];
     jint ret = 0;
 
-    // TODO: implementation
+    DthingTraceD("keypad: init and unregister key message callback\n");
 
+#if defined(ARCH_ARM_SPD)
+    SCI_UnregisterMsg(KEYPAD_SERVICE,
+                     (uint8)(KPD_DOWN & 0xff),
+                     (uint8)(KPD_UP & 0xff),
+                     NULL);
+#endif //ARCH_ARM_SPD
     RETURN_INT(ret);
 }
 
 #if defined(ARCH_ARM_SPD)
+/**
+ * @brief callback when keypad is pressed
+ *
+ * @param id Server ID, EXAMPLE: KEYPAD_SERVICE
+ * @param argc ui_event_type, Message ID of the registered event
+ * @param argv signal_ptr, Signal Sent to Client
+ */
+void kpd_callback (uint32 id, uint32 argc, void *argv)
+{
+  uint32  key_code = 0;
+  uint16  event_code = 0;
+
+  DthingTraceD("keypad: kpd_callback\n");
+
+  if (g_keyEventNotifier != NULL)
+  {
+    if (id == KEYPAD_SERVICE)
+    {
+      key_code = (uint32)(argv);
+      event_code = (uint16)(argc & 0xFFFF);
+
+      switch(event_code)
+      {
+      case KPD_DOWN:
+        g_keyEventNotifier->notified = key_code;
+        DthingTraceD("keypad: KPD_DOWN %d\n", key_code);
+        break;
+      case KPD_UP:
+        g_keyEventNotifier->notified = key_code;
+        DthingTraceD("keypad: KPD_UP %d\n", key_code);
+        break;
+      default:
+        g_keyEventNotifier->notified = 0;
+        DthingTraceD("keypad: Unknown key event %d\n", event_code);
+        break;
+      }
+      AsyncIO_notify(g_keyEventNotifier);
+    }
+  }
+}
+
 static jlong convertNativeKeyToJava(KPDSVR_SIG_T *key_sig_ptr)
 {
   jlong keyCode = 0;
