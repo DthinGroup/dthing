85191b7bd8a78250955ed01287902610ab253c3e
fei <superfei2002@163.com>
2015-08-12 18:15:37 +0800

    [demo] add smartshoe demo trial version 0.1

---
+++ demo/SmartShoe/SmartShoe.java
@@ -0,0 +1,336 @@
+import jp.co.cmcc.event.Applet;
+import jp.co.cmcc.event.Event;
+
+import java.net.http.HttpURLConnection;
+import java.net.http.URL;
+import java.io.IOException;
+import java.io.InputStream;
+
+import iot.oem.gpio.Gpio;
+import iot.oem.i2c.I2CManager;
+import iot.oem.comm.CommConnectionImpl;
+
+
+public class SmartShoe extends Applet {
+
+    private static final String REPORT_SERVER_FORMAT = "http://42.121.18.62:8080/dthing/ParmInfo.action?saveDataInfo&saveType=log&parmInfo=";
+    private static boolean allowLogPrint = true;
+    private static boolean allowRunning = true;
+    protected static String longitude = "100.000001";
+    protected static String latitude = "36.000001";
+    protected static int stepcount = 0;
+    private static I2CManager manager = null;
+    private static int busId = 2;
+    private static int slaveAddress = 0x30;
+    private static int regAddressNumber = 1;
+    private static int subAccAddress = 0x02; //sub address for accelerator data read
+    private static int accRange = 0;
+
+    public void cleanup() {
+        allowRunning = false;
+    }
+
+    public void processEvent(Event paramEvent) {
+    }
+    
+    public void startup() {
+    	startupGPSThread();
+    	startupGSensorThread();
+
+    	while(allowRunning) {
+            try {
+            	log("longitude:" + longitude + ";latitude:" + latitude + ";stepcount:" + stepcount);
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				log("Exception:" + e);
+			}
+    	}
+    }
+    
+    public void startupGPSThread() {
+        new Thread() {
+            public void run() {
+                try {
+                    Gpio ldo = new Gpio(60); //60 for board, 7 for shoe
+                    ldo.setCurrentMode(Gpio.WRITE_MODE);
+                    ldo.write(true);
+                    log("pull GPIO 60 to high");
+                } catch (IllegalArgumentException e1) {
+                    log("IllegalArgumentException:" + e1);
+                } catch (IOException e1) {
+                    log("IOException:" + e1);
+                }
+
+                CommConnectionImpl gpsComm = CommConnectionImpl.getComInstance(0, 9600);
+                try {
+                    byte[] buf = new byte[128];
+
+                    InputStream is = gpsComm.openInputStream();
+                    int readSize;
+                    do {
+                        try {
+                            Thread.sleep(10000L);
+                        } catch (InterruptedException e) {
+                        	log("InterruptedException:" + e);
+                        }
+
+                        readSize = is.read(buf, 0, 128);
+
+                        if (readSize < 0)
+                        {
+                        	log("exit when readSize is less than 0");
+                            break;
+                        }
+
+                        String readString = new String(buf);
+
+                        //log("read:" + convertEscapedChar(readString));
+                    } while (allowRunning);
+                    gpsComm.close();
+                    notifyDestroyed();
+                } catch (IOException e) {
+                	log("IOException:" + e);
+                }
+            }
+
+            private String convertEscapedChar(String original)
+            {
+                String escaped = "";
+                char[] ctest = original.toCharArray();
+                for (int i = 0; i < ctest.length; i++)
+                {
+                    if ((ctest[i] != '\r') && (ctest[i] != '\n'))
+                    {
+                        escaped = escaped + ctest[i];
+                    }
+                    else
+                    {
+                        escaped = escaped + '.';
+                    }
+                }
+                return escaped;
+            }
+        }.start();
+    }
+    
+    public void startupGSensorThread() {
+        try {
+			startGSensor();
+	        AcceleratorTest();
+		} catch (IOException e) {
+			log("IOException:" + e);
+		}
+    }
+    
+    private void startGSensor() throws IOException
+    {
+        manager = new I2CManager(busId, I2CManager.DATA_RATE_FAST, slaveAddress, regAddressNumber);
+        initGSensor();
+    }
+
+    private void resetGSensor()
+    {
+        //TODO:
+    }
+
+    private void stopGSensor() throws IOException
+    {
+        manager.destroy();
+        manager = null;
+    }
+
+    private void initGSensor() throws IOException
+    {
+        byte[] reg = new byte[1];
+        byte[] buff = new byte[1];
+
+        //Disable all interrupt (See 5.11 Interrupt settings)
+        reg[0] = 0x16;
+        buff[0] = 0x00;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0], buff);
+
+        //contains the definition of the theta blocking angle for the orientation interrupt.
+        reg[0] = 0x2D;
+        buff[0] = 0x01;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0],buff);
+
+        //contains the interrupt reset bit and the interrupt mode selection.
+        reg[0] = 0x21;
+        buff[0] = 0x03; //0x03 means temporary, 1s
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0], buff);
+
+        //maps all interrupts to INT1 pin
+        reg[0] = 0x19;
+        buff[0] = (byte)0xF7;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0], buff);
+
+        //contains the behavioural configuration
+        reg[0] = 0x20;
+        buff[0] = 0x01;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0], buff);
+
+        //contains the threshold definition for the slope interrupt.
+        reg[0] = 0x28;
+        buff[0] = 0x08;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0],buff);
+
+        //contains the definition of the number of samples to be evaluated for the slope interrupt (any-motion detection).
+        reg[0] = 0x27;
+        buff[0] = 0x00;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0],buff);
+
+        //enables slope interrupt for x-axis, y-axis, z-axis and flat interrupt (See 5.11 Interrupt settings)
+        reg[0] = 0x16;
+        buff[0] = 0x47;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0], buff);
+    }
+
+    private void initAccelerator() throws IOException
+    {
+        byte[] reg = new byte[1];
+        byte[] buff = new byte[1];
+
+        //set acceleration range to +-2g (See 5.7 g-range selection)
+        reg[0] = 0x0f;
+        buff[0] = 0x03;
+        accRange = 2;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0], buff);
+
+        //set data collection and output, MSB data depends on LSB data (See 5.10 Special control settings)
+        reg[0] = 0x13;
+        buff[0] =0x0;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0], buff);
+
+        //set filter data bandwidth to 1000HZ (See 5.8 Bandwidths)
+        reg[0] = 0x10;
+        buff[0] = 0x10;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0], buff);
+
+        //set power modes, no delay, lower volumn mode and 50ms to sleep (See 5.9 Power modes)
+        reg[0] = 0x11;
+        buff[0] = 0x58;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0], buff);
+    }
+
+    private void enableAccelerator() throws IOException
+    {
+        byte[] reg = new byte[1];
+        byte[] buff = new byte[1];
+
+        //According to WriteG_Reg implementation, write [0x30, 0x02, 0x31] will start to read accelerate information
+        reg[0] = 0x02;
+        buff[0] = 0x31;
+        manager.send(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, reg[0], buff);
+    }
+
+    //accValue = sign + valueHead + "." + valueTail
+    //sign = "-" or ""
+    public String getAccValue(int lsb, int msb)
+    {
+        String accValue = null;
+        int sign = (msb & 0x80) >> 7;
+        int value = ((msb & 0x7f) << 2) + ((lsb & 0xc0) >> 6);
+        int valueHead = value * accRange / 512;
+        int valueTail = (value * accRange % 512) * 1000 / 512;
+        accValue = ((sign > 0)? "-" : "") + valueHead + "." + valueTail;
+        return accValue;
+    }
+
+    //accValue = sign + valueHead + "." + valueTail
+    //sign = "-" or ""
+    public int getAccIntValue(int lsb, int msb)
+    {
+        int accValue = 0;
+        int sign = (msb & 0x80) >> 7;
+        int value = ((msb & 0x7f) << 2) + ((lsb & 0xc0) >> 6);
+        int valueHead = value * accRange / 512;
+        int valueTail = (value * accRange % 512) * 1000 / 512;
+        accValue = valueHead * 100 + valueTail;
+  
+        if (sign > 0) {
+        	accValue = -accValue;
+        }
+        return accValue;
+    }
+    
+    private void AcceleratorTest() throws IOException
+    {
+        initAccelerator();
+        enableAccelerator();
+        //data collector thread
+        new Thread() {
+            byte[] buf = new byte[6];
+            /*
+            String xStr = null;
+            String yStr = null;
+            String zStr = null;
+            */
+            int xAc = 0;
+            int yAc = 0;
+            int zAc = 0;
+
+            public void run() {
+            	StepCounter counter = new StepCounter();
+
+                while(allowRunning) {
+                    try {
+                        manager.receive(slaveAddress, I2CManager.ADDRESS_TYPE_7BIT, subAccAddress, buf);
+                        /*
+                        xStr = getAccValue(buf[0], buf[1]);
+                        yStr = getAccValue(buf[2], buf[3]);
+                        zStr = getAccValue(buf[4], buf[5]);
+                        */
+                        xAc = getAccIntValue(buf[0], buf[1]);
+                        yAc = getAccIntValue(buf[2], buf[3]);
+                        zAc = getAccIntValue(buf[4], buf[5]);
+                        counter.saveAccValue(xAc, yAc, zAc);
+
+                        if (counter.available()) {
+                        	stepcount += counter.fetchStepCount();
+                        }
+
+                    } catch (IllegalArgumentException e) {
+                        log("IllegalArgumentException:" + e);
+                    } catch (NullPointerException e) {
+                        log("NullPointerException:" + e);
+                    } catch (IOException e) {
+                        log("IOException" + e);
+                    }
+                }
+                try {
+					stopGSensor();
+				} catch (IOException e) {
+					log("IOException:" + e);
+				}
+                notifyDestroyed();
+            }
+        }.start();
+    }
+    
+    private void reportTestInfo(String msg) throws IOException
+    {
+        String content = "GSensor:" + msg.replace(' ', '.');
+        String reportInfo = REPORT_SERVER_FORMAT + content;
+
+        if (allowLogPrint)
+        {
+            System.out.println(content);
+        }
+
+        URL url = new URL(reportInfo);
+        HttpURLConnection httpConn = (HttpURLConnection)url.openConnection();
+        httpConn.setRequestMethod(HttpURLConnection.POST);
+        InputStream dis = httpConn.getInputStream();
+        dis.close();
+        httpConn.disconnect();
+    }
+
+    private void log(String msg)
+    {
+        try {
+            reportTestInfo(msg);
+        } catch (IOException e) {
+            System.out.println("IOException:" + e);
+        }
+    }
+}
---
+++ demo/SmartShoe/StepCounter.java
@@ -0,0 +1,260 @@
+/**
+ * This class is used for calculating steps through acceleration data,
+ * which supports both two axis accelerometer and three axis accelerometer.
+ * According to the constructor of StepCounter, there are below tow ways of using StepCounter class:
+ *      StepCounter counter = new StepCounter();
+ *      counter.saveAccValue(xInAc[i], yInAc[i], zInAc[i]);
+ *      if (counter.available()) {
+ *          totalStep += counter.fetchStepCount();
+ *      }
+ */
+public class StepCounter {
+    private boolean DEBUG = false;
+    private boolean isBadStep = false;
+
+    private static final int UNDEFINED_VALUE = -10;
+    private static final int X_AXIS_MAX = 0;
+    private static final int Y_AXIS_MAX = 100;
+    private static final int Z_AXIS_MAX = 200;
+    private static final int MIN_VALID_STEP_GAP = 30;
+    private static final int MIN_STEP_DURATION = 20;
+    private static final int MAX_STEP_DURATION = 200;
+    private int minXAc = UNDEFINED_VALUE;
+    private int minYAc = UNDEFINED_VALUE;
+    private int minZAc = UNDEFINED_VALUE;
+    private int maxXAc = UNDEFINED_VALUE;
+    private int maxYAc = UNDEFINED_VALUE;
+    private int maxZAc = UNDEFINED_VALUE;
+
+    /** Whose axis has max data. */
+    private int maxAxis = 0;
+    /** The frequency of data output. */
+    private final int DATA_OUTPUT_FREQ = 5;
+    /** Caculate threshold dynamically every 10 output data. */
+    private final int SAMPLING_NUMBER = 10;
+    /** sampling counter, reset to 0 every SAMPLING_NUMBER times. */
+    private int samCounter = 0;
+    /** x axis dynamic threshold. */
+    private int xDcThred = UNDEFINED_VALUE;
+    /** y axis dynamic threshold. */
+    private int yDcThred = UNDEFINED_VALUE;
+    /** z axis dynamic threshold. */
+    private int zDcThred = UNDEFINED_VALUE;
+
+    /** Is three axis accelerometer. */
+    private boolean isThreeAxisAcc = false;
+
+    /** The state of calculating step. */
+    private static final int STEP_INITIALIZING = 0;
+    private static final int STEP_SEARCHING = 100;
+    private static final int STEP_WORKING = 200;
+
+    /**
+     * The constructor of StepCounter with no argument.
+     * In this way, you need to speccify the data of axises acceleration at caculateSteps.
+     */
+    public StepCounter() {
+        initAcData();
+    }
+
+    /** Initialize data. */
+    private void initAcData() {
+        samCounter = 0;
+        minYAc = UNDEFINED_VALUE;
+        minXAc = UNDEFINED_VALUE;
+        minZAc = UNDEFINED_VALUE;
+        maxXAc = UNDEFINED_VALUE;
+        maxYAc = UNDEFINED_VALUE;
+        maxZAc = UNDEFINED_VALUE;
+        xDcThred = UNDEFINED_VALUE;
+        yDcThred = UNDEFINED_VALUE;
+        zDcThred = UNDEFINED_VALUE;
+    }
+
+    /** Reset data for another cycle of sampling. */
+    private void resetSamplingData() {
+        samCounter = 0;
+        /** Caculate dynamic threshold value. */
+        xDcThred = (maxXAc+minXAc)/2;
+        yDcThred = (maxYAc+minYAc)/2;
+        if (isThreeAxisAcc) {
+            zDcThred = (maxZAc+minZAc)/2;
+        }
+
+        if (maxXAc-minXAc >= MIN_VALID_STEP_GAP
+            || maxYAc-minYAc >= MIN_VALID_STEP_GAP
+            || (isThreeAxisAcc && maxZAc-minZAc >= MIN_VALID_STEP_GAP)) {
+            isBadStep = false;
+            /** Find the axis whose acceleration changes is the largest. */
+            if (isThreeAxisAcc) {
+                if (maxXAc-minXAc > maxYAc-minYAc) {
+                    if (maxXAc-minXAc > maxZAc-minZAc) {
+                        maxAxis = X_AXIS_MAX;
+                    } else {
+                        maxAxis = Z_AXIS_MAX;
+                    }
+                } else if (maxYAc-minYAc > maxZAc-minZAc) {
+                    maxAxis = Y_AXIS_MAX;
+                } else {
+                    maxAxis = Z_AXIS_MAX;
+                }
+            } else {
+                maxAxis = (maxXAc-minXAc > maxYAc-minYAc) ? X_AXIS_MAX : Y_AXIS_MAX;
+            }
+        } else {
+            isBadStep = true;
+        }
+
+        log("maxXAc = " + maxXAc + ",minXAc = " + minXAc
+            + " maxYAc = " + maxYAc + ", minYAc = " + minYAc
+            + (isThreeAxisAcc ? (" maxZAc = " + maxZAc + ", minZAc = " + minZAc) : ""));
+        log("xDcThred = " + xDcThred + ", yDcThred = " + yDcThred
+            + (isThreeAxisAcc ? (", zDcThred = " + zDcThred) : "")
+            + ", isBadStep = " + isBadStep + ", maxAxis = " + maxAxis);
+
+        minXAc = UNDEFINED_VALUE;
+        minYAc = UNDEFINED_VALUE;
+        minZAc = UNDEFINED_VALUE;
+        maxXAc = UNDEFINED_VALUE;
+        maxYAc = UNDEFINED_VALUE;
+        maxZAc = UNDEFINED_VALUE;
+    }
+    
+    /**
+     * Start Caculating steps with the acceleration data.
+     * When you specify zInAc as non-null value, it means three axis accelerometer.
+     * When you specify zInAc as non-null value, it means three axis accelerometer.
+     */
+    private int caculateSteps(int xAc[], int yAc[], int zAc[]) {
+        int i = 0;
+        int lastStepPoint = 0;
+        int stepState = STEP_INITIALIZING;
+        int dataLen = 0;
+        int stepCount = 0;
+
+        if (zAc != null && zAc.length > 0) {
+            log("This is a three axis accelerometer!");
+            isThreeAxisAcc = true;
+        } else { // only two axis accelerometer
+            log("This is a two axis accelerometer!");
+            isThreeAxisAcc = false;
+        }
+
+        if (xAc.length > yAc.length) {
+            dataLen = !isThreeAxisAcc ? xAc.length : ((zAc.length > yAc.length) ? yAc.length : zAc.length);
+        } else {
+            dataLen = !isThreeAxisAcc ? yAc.length : ((zAc.length > xAc.length) ? xAc.length : zAc.length);
+        }
+
+        log("dataLen = " + dataLen);
+
+        initAcData();
+        for (i=0; i<dataLen; i++) {
+            /** Find the max and min value in axis. */
+            if (maxXAc == UNDEFINED_VALUE || xAc[i] > maxXAc) {
+                maxXAc = xAc[i];
+            }
+            if (minXAc == UNDEFINED_VALUE || xAc[i] < minXAc) {
+                minXAc = xAc[i];
+            }
+            if (maxYAc == UNDEFINED_VALUE || yAc[i] > maxYAc) {
+                maxYAc = yAc[i];
+            }
+            if (minYAc == UNDEFINED_VALUE || yAc[i] < minYAc) {
+                minYAc = yAc[i];
+            }
+            if (isThreeAxisAcc && (maxZAc == UNDEFINED_VALUE || zAc[i] > maxZAc)) {
+                maxZAc = zAc[i];
+            }
+            if (isThreeAxisAcc && (minZAc == UNDEFINED_VALUE || zAc[i] < minZAc)) {
+                minZAc = zAc[i];
+            }
+
+            samCounter++;
+            /** Caculate threshold value and reset sampling data. */
+            if (samCounter == SAMPLING_NUMBER) {
+                resetSamplingData();
+            }
+
+            if (isBadStep || i == 0) {
+                continue;
+            }
+
+            /** Calutlating steps.  */
+            if ((maxAxis==X_AXIS_MAX && xAc[i-1]>xDcThred && xDcThred>xAc[i])
+                || (maxAxis==Y_AXIS_MAX && yAc[i-1]>yDcThred && yDcThred>yAc[i])
+                || (isThreeAxisAcc && maxAxis == Z_AXIS_MAX && zAc[i-1]>zDcThred && zDcThred>zAc[i])) {
+                if ((i-lastStepPoint) < (int)(MIN_STEP_DURATION/DATA_OUTPUT_FREQ)) {
+                    log("ignore: lastStepPoint = " + lastStepPoint + ", i = " + i);
+                    continue; // Ignore because of step gap is too short.
+                } else if ((i-lastStepPoint) > (int)(MAX_STEP_DURATION/DATA_OUTPUT_FREQ)) {
+                    stepState = STEP_SEARCHING;
+                    lastStepPoint = i;
+                } else {
+                    if (stepState == STEP_INITIALIZING) {
+                        stepState = STEP_SEARCHING;
+                    } else if (stepState == STEP_SEARCHING) {
+                        stepState = STEP_WORKING;
+                        stepCount = stepCount + 2;
+                    } else {
+                        stepCount = stepCount + 1;
+                    }
+
+                    lastStepPoint = i;
+                }
+            }
+        }
+
+        log("You complete " + stepCount + " steps.");
+
+        return stepCount;
+    }
+
+    private void log(String logtxt)
+    {
+        if(DEBUG)
+        {
+            System.out.println("StepCounter: "+logtxt+"\n");
+        }
+    }
+
+    private static final int maxArraySize = 50;
+    private int[] xArray = new int[maxArraySize];
+    private int[] yArray = new int[maxArraySize];
+    private int[] zArray = new int[maxArraySize];
+    private int cursor = 0;
+    private int caculatedStepCount = 0;
+    
+    private void resetArray() {
+    	cursor = 0;
+		xArray = new int[maxArraySize];
+		yArray = new int[maxArraySize];
+		zArray = new int[maxArraySize];	
+    }
+    
+    public void saveAccValue(int xAc, int yAc, int zAc) {
+    	if (cursor >= maxArraySize) {
+    		caculatedStepCount = caculateSteps(xArray, yArray, zArray);
+    		resetArray();
+    	}
+
+    	xArray[cursor] = xAc;
+    	yArray[cursor] = yAc;
+    	zArray[cursor] = zAc;
+    	cursor++;
+    }
+    
+    public boolean available() {
+    	return ((caculatedStepCount > 0) || (cursor >= maxArraySize));
+    }
+    
+    public int fetchStepCount() {
+    	int result = caculatedStepCount;
+    	if (cursor >= maxArraySize) {
+    		result += caculateSteps(xArray, yArray, zArray);
+    		caculatedStepCount = 0;
+    		resetArray();
+    	}
+    	return result;
+    }
+}
