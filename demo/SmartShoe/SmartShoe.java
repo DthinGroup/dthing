75f1621807736c86147f2ee6edb26a0ac33418a8
爱德华.许 <Edward.Xu@yarlungsoft.com>
2015-08-15 11:56:48 +0800

    tmp version

--- demo/SmartShoe/SmartShoe.java
+++ demo/SmartShoe/SmartShoe.java
@@ -122,8 +122,9 @@ public class SmartShoe extends Applet {
 
 	public void convertgpsBuftoStringBuf(){
 		
+		log("entry of convertgpsBuftoStringBuf");
 		for (int i=0;i<128;i++){
-			gpsStrBuf.replace(i, i+1, (char)(gpsBuf[i] & 0xff));
+			gpsStrBuf.setCharAt(i, (char)(gpsBuf[i] & 0xff));
 		}
 		return;
 	}
