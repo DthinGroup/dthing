96fd974afe3068294e8db948ec4888cf1f120ed5
爱德华.许 <Edward.Xu@yarlungsoft.com>
2015-08-17 23:59:51 +0800

    tmp version

--- demo/SmartShoe/SmartShoe.java
+++ demo/SmartShoe/SmartShoe.java
@@ -211,7 +211,7 @@ public class SmartShoe extends Applet {
             xAc = getAccIntValue(accBuf[0], accBuf[1]);
             yAc = getAccIntValue(accBuf[2], accBuf[3]);
             zAc = getAccIntValue(accBuf[4], accBuf[5]);
-            log("11 save value:" + xAc + ":" + yAc + ":" + zAc);
+            log("12 save value:" + xAc + ":" + yAc + ":" + zAc);
 			
             counter.saveAccValue(xAc, yAc, zAc);
             if (counter.available()) {
