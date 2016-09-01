8060d62e73976b8b51b73cde4c9b31f05abf7f3c
nix.long <nixlong@126.com>
2016-09-01 18:18:09 +0800

    changes

---
+++ java/src/com/yarlungsoft/iot/mqttv3/simple/Mqtt.java
@@ -0,0 +1,57 @@
+package com.yarlungsoft.iot.mqttv3.simple;
+
+import com.yarlungsoft.iot.mqttv3.MqttException;
+import com.yarlungsoft.iot.mqttv3.simple.ISimpleMqttClient;
+import com.yarlungsoft.iot.mqttv3.simple.SimpleMqttClient;
+import com.yarlungsoft.util.Log;
+
+import jp.co.cmcc.event.Applet;
+import jp.co.cmcc.event.Event;
+
+
+public class Mqtt extends Applet {
+	private SimpleMqttClient mqtt;
+	public Mqtt() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void cleanup() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void processEvent(Event arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void startup() {
+			
+		mqtt = SimpleMqttClient.getInstance();
+		Log.log("Mqtt test", "thread startup");
+		new Thread(new Runnable() {		
+			
+			public void run() {
+				try {
+					String url = "tcp://182.61.25.208:1883";
+					Log.log("Mqtt test", "ready to connect - " + url);
+					int ret = mqtt.connect("tcp://182.61.25.208:1883");
+					Log.log("Mqtt test", "connect result - " + ret);
+					
+					if(ret == ISimpleMqttClient.SUCCESS){
+						Log.log("Mqtt test", "ready to subscribe");
+						ret = mqtt.subscribe("fuck years", null);
+						Log.log("Mqtt test", "subscribe ret - " + ret);
+					}
+					
+				} catch (MqttException e) {
+					e.printStackTrace();
+				} catch (Exception e){
+					
+				}
+			}
+		}).start();
+	
+	}
+
+}
